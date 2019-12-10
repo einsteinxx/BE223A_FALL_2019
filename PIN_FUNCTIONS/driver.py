5a88d1fc9a9cf3c46e202c129ec634b1df1c2bd3
keane <keane.gonzalez@gmail.com>
2019-12-10 00:54:17 -0800

    Copy from class folder

---
+++ PIN_FUNCTIONS/driver.py
@@ -0,0 +1,3 @@
+import main as get_pin_locations
+
+get_pin_locations(input_directory = '/home/kgonzalez/BE223A_2019/data/', output_image_base = '/home/kgonzalez/BE223A_2019/CT_Segmentation/PIN_NII', nifti_out_folder = '/home/kgonzalez/BE223A_2019/CT_Segmentation/PIN_NII')
